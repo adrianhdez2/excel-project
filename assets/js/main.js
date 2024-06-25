37a45f7ed7222d9b0bcdf87465cdb9f8cae05b1f
adrianhdez2 <cesarahh3@gmail.com>
2024-06-25 17:02:32 -0600

    Add some changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,12 +2,27 @@ import { Excel, ExcelViewer, FormatExcel, TableClear } from "./classes.js"
 const $excelFile = document.getElementById('excel-file')
 const $btnLoadDocument = document.getElementById('load-document')
 const $labelName = document.querySelector("#select-excel-file span")
+const $sheetSelect = document.getElementById('sheet-select');
+const $sheetSelectContainer = document.querySelector('.select-excel-sheets')
 const tableID = 'excel-table'
+let sheetNames = null
+let workbook = null
 let loading = false
 
-$excelFile.onchange = function () {
+$excelFile.onchange = async function () {
     $labelName.textContent = $excelFile.files[0].name
     $btnLoadDocument.removeAttribute('disabled')
+}
+
+function loadSheet(workbook, sheetName) {
+    const worksheet = workbook.Sheets[sheetName];
+    const jsonData = XLSX.utils.sheet_to_json(worksheet, { header: 1 });
+
+
+    const formattedData = new FormatExcel(jsonData).formatNumber()// --> Corregir formato de string a números
+
+    const excel = new Excel(formattedData)
+    ExcelViewer.viewer(tableID, excel)
 }
 
 $btnLoadDocument.onclick = async function () {
@@ -17,11 +32,18 @@ $btnLoadDocument.onclick = async function () {
 
     TableClear.clear(tableID) // --> Limpiar la tabla antes de cargar el documento
 
-    let content = await readXlsxFile($excelFile.files[0])
-    content = new FormatExcel(content).formatNumber() // --> Corregir formato de string a numeros
+    const data = await $excelFile.files[0].arrayBuffer();
+    workbook = XLSX.read(data, { type: 'array' });
+    sheetNames = workbook.SheetNames;
 
-    const excel = new Excel(content)
-    ExcelViewer.viewer(tableID, excel)
+    if (sheetNames.length > 1) {
+        createSelect()
+
+        loadSheet(workbook, sheetNames[0]); // Cargar la primera hoja por defecto
+    } else {
+        $sheetSelect.style.display = 'none';
+        loadSheet(workbook, sheetNames[0]);
+    }
 
     loadText()
 
@@ -48,4 +70,20 @@ function loadText() {
 
     $btnLoadDocument.removeAttribute('disabled')
     $btnLoadDocument.textContent = 'Cargar'
+}
+
+function createSelect() {
+    $sheetSelectContainer.style.display = 'flex';
+    $sheetSelect.innerHTML = ''; // Limpiar select previo
+    sheetNames.forEach(sheetName => {
+        const option = document.createElement('option');
+        option.value = sheetName;
+        option.textContent = sheetName;
+        $sheetSelect.appendChild(option);
+    });
+
+    $sheetSelect.onchange = () => {
+        TableClear.clear(tableID);
+        loadSheet(workbook, $sheetSelect.value);
+    };
 }
